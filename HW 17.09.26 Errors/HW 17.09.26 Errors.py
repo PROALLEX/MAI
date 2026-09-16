a44d6dd27a1af92b275bfacb673c1f95564010a2
Алексий <callofdutymobile200821@gmail.com>
2026-09-17 01:52:18 +0300

    HW 17.09.26 Errors
    HW 17.09.26 Errors

---
+++ HW 17.09.26 Errors/HW 17.09.26 Errors.py
@@ -0,0 +1,13 @@
+a=int(input(f"для выбора ошибки напишите число от 1 до 4 \n"))
+if a==1:
+    # 1. (SyntaxError)
+    print("Привет, мир )")
+elif a==2:
+    # 2. (RuntimeError / Exception)
+    print(10 / 0)
+elif a==3:
+    # 3. Семантическая ошибка (SemanticError)
+    print(unknown)
+elif a==4:
+    # 4. (LogicalError)
+    print(4 + 8 / 2)
